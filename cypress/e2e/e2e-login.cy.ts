0dc1f4f28ca37b358befc26ea5935647cbae30a6
Deepak Jha <deepakjha14.ex@gmail.com>
2024-09-11 21:15:21 +0530

    changed the location of the tedt.csv file

--- cypress/e2e/e2e-login.cy.ts
+++ cypress/e2e/e2e-login.cy.ts
@@ -38,7 +38,7 @@ describe(
         it(
             'Should login in the app using csv file data',
             () => {
-                cy.readFile('cypress/fixtures/test.csv',  'utf8' ).then(
+                cy.readFile('cypress/downloads/test.csv').then(
                     (data) => {
                         cy.task('csvToJson', data).then(
                             (jsonData: any) => {
